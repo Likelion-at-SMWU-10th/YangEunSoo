0ed1911f98138d190b6da91bc8299f798627e3df
eunxoo <dmstn7989@naver.com>
2022-07-14 00:34:57 +0900

    리액트 라우터로 페이지 연결, 상위 element와 하위 element 동시에 보이기

---
+++ week10/week6_router/src/pages/WhoamI.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const whoAmI = () => {
+  return (
+    <div>
+      <h1>자기소개</h1>
+      {/*내 소개 자유롭게 적기*/}
+      <ol>
+        <li>이름 : 양은수</li>
+        <li>취미 : 영화 보기</li>
+        <li>최근의 관심사 : 여자 배구</li>
+      </ol>
+    </div>
+  );
+};
+
+export default whoAmI;
